156107788fbd1b64e32c31a1513476d5e88b3674
Fomenko-Denis <fomenko.roust@mail.ru>
2023-04-02 16:45:46 +0800

    initial commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,11 +1,12 @@
 package ru.netology;
+
 public class Main {
 
 
- public static void main(String[]args){
+    public static void main(String[] args) {
 
-BonusService service = new BonusService();
-     long bonus = service.calculate(5000,true);
-     System.out.println(bonus);
-        }
+        BonusService service = new BonusService();
+        long bonus = service.calculate(5000, true);
+        System.out.println(bonus);
+    }
 }
